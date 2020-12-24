cbee0904037db94380144afe67dae45752f783ca
furui <r.fu@bimfoo.cn>
2020-12-24 18:26:15 +0800

    fix(*): 数据修改功能新增加

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   CounterProvider _counterProvider = new CounterProvider();
+  CounterProvider _counterProvider2 = new CounterProvider();
 
   var data1;
   var option = {
@@ -30,57 +31,57 @@ class _MyAppState extends State<MyApp> {
       }
     ]
   };
+  var option2 = {
+    "tooltip": {"trigger": 'item', "formatter": '{a} <br/>{b}: {c} ({d}%)'},
+    "legend": {
+      "orient": 'vertical',
+      "left": 10,
+      "data": ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+    },
+    "series": [
+      {
+        "name": '访问来源',
+        "type": 'pie',
+        "radius": ['50%', '70%'],
+        "avoidLabelOverlap": false,
+        "label": {"show": false, "position": 'center'},
+        "emphasis": {
+          "label": {"show": true, "fontSize": '30', "fontWeight": 'bold'}
+        },
+        "labelLine": {"show": false},
+        "data": [
+          {"value": 335, "name": '直接访问'},
+          {"value": 310, "name": '邮件营销'},
+          {"value": 234, "name": '联盟广告'},
+          {"value": 135, 'name': '视频广告'},
+          {"value": 1548, "name": '搜索引擎'}
+        ]
+      }
+    ]
+  };
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _counterProvider.refresh(UniqueKey(), option);
+    _counterProvider2.refresh(UniqueKey(), option2);
   }
 
   @override
   Widget build(BuildContext context) {
-    var option2 = {
-      "tooltip": {"trigger": 'item', "formatter": '{a} <br/>{b}: {c} ({d}%)'},
-      "legend": {
-        "orient": 'vertical',
-        "left": 10,
-        "data": ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-      },
-      "series": [
-        {
-          "name": '访问来源',
-          "type": 'pie',
-          "radius": ['50%', '70%'],
-          "avoidLabelOverlap": false,
-          "label": {"show": false, "position": 'center'},
-          "emphasis": {
-            "label": {"show": true, "fontSize": '30', "fontWeight": 'bold'}
-          },
-          "labelLine": {"show": false},
-          "data": [
-            {"value": 335, "name": '直接访问'},
-            {"value": 310, "name": '邮件营销'},
-            {"value": 234, "name": '联盟广告'},
-            {"value": 135, 'name': '视频广告'},
-            {"value": 1548, "name": '搜索引擎'}
-          ]
-        }
-      ]
-    };
-
-    return ChangeNotifierProvider(
-        builder: (context) => _counterProvider,
-        child: new MaterialApp(
-          home: new Scaffold(
-              appBar: new AppBar(
-                title: const Text('Native WebView as Widget'),
-              ),
-              body: new SingleChildScrollView(
-                child: new Column(
-                  children: <Widget>[
-                    new Text('Native WebView as Widget\n\n'),
-                    Consumer(builder: (BuildContext context,
+    return new MaterialApp(
+      home: new Scaffold(
+          appBar: new AppBar(
+            title: const Text('Native WebView as Widget'),
+          ),
+          body: new SingleChildScrollView(
+            child: new Column(
+              children: <Widget>[
+                new Text('Native WebView as Widget\n\n'),
+                ChangeNotifierProvider(
+                    builder: (context) => _counterProvider,
+                    child: Consumer(builder: (BuildContext context,
                         CounterProvider counterProvider, Widget child) {
                       print('EchartView。。。。。。');
                       return new Container(
@@ -91,18 +92,40 @@ class _MyAppState extends State<MyApp> {
                         height: 300.0,
                         width: 500.0,
                       );
-                    }),
-                    Builder(builder: (context) {
-                      return RaisedButton(
-                        child: Text("更改数据"),
-                        onPressed: () {
-                          _counterProvider.refresh(UniqueKey(), option2);
-                        },
+                    })),
+                Builder(builder: (context) {
+                  return RaisedButton(
+                    child: Text("更改数据"),
+                    onPressed: () {
+                      _counterProvider.refresh(UniqueKey(), option2);
+                    },
+                  );
+                }),
+                ChangeNotifierProvider(
+                    builder: (context) => _counterProvider2,
+                    child: Consumer(builder: (BuildContext context,
+                        CounterProvider counterProvider, Widget child) {
+                      print('EchartView2。。。。。。');
+                      return new Container(
+                        child: EchartView(
+                            key: _counterProvider2.keyCount,
+                            height: 300,
+                            data: counterProvider.value),
+                        height: 300.0,
+                        width: 500.0,
                       );
-                    }),
-                  ],
-                ),
-              )),
-        ));
+                    })),
+                Builder(builder: (context) {
+                  return RaisedButton(
+                    child: Text("更改数据2"),
+                    onPressed: () {
+                      _counterProvider2.refresh(UniqueKey(), option);
+                    },
+                  );
+                }),
+              ],
+            ),
+          )),
+    );
   }
 }
--- lib/echart_provider.dart
+++ lib/echart_provider.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 class CounterProvider with ChangeNotifier {
   Key key;
+
   Map data;
 
   Map get value => data;
 
+ 
   Key get keyCount => key;
 
   void refresh(Key key, Map data) {
